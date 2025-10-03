e36e671bfc38cb16aa9dfa8b61a2e77e21e3cf31
NegativeZ1 <rdzcuatriz2663@gmail.com>
2025-10-03 12:08:31 +0800

    add to my file

---
+++ facebook/lib/widgets/createpost.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CreatePost extends StatelessWidget {
+  final VoidCallback? onPostCreate;
+  const CreatePost({super.key, this.onPostCreate});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onPostCreate,
+      leading: const CircleAvatar(
+        backgroundImage: AssetImage("assets/profile/prof2.jpg"),
+      ), title: const TextField(
+        decoration: InputDecoration(
+          hintText: "What's on your mind?",
+          border: InputBorder.none,
+        ),
+      ),
+      trailing: const Icon(Icons.image)
+    );
+  }
+}
